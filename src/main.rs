ff68e9fc05d815767027e70356c343c2c285e742
wulang <wulang@szlhxx.com>
2024-09-18 16:29:13 +0800

    ✨: 分离组件/全局回调

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ fn main() -> Result<(), PlatformError> {
     let todo_model = Rc::new(VecModel::<TodoItem>::from(vec![]));
 
     let app = AppWindow::new()?;
-    let ui_handle = app.as_weak().unwrap();
 
-    ui_handle.set_dt_text(today().into());
-    ui_handle.on_todo_add({
+    let today = today();
+    app.set_dt_text(today.into());
+
+    app.on_todo_add({
         let todo_model = todo_model.clone();
         move |text| {
             todo_model.push(TodoItem {
@@ -23,7 +24,6 @@ fn main() -> Result<(), PlatformError> {
         }
     });
 
-    ui_handle.set_todo_list(todo_model.clone().into());
-
+    app.set_todo_list(todo_model.clone().into());
     app.run()
 }
