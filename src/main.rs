08dd6c3a1547bc5d7f17d5c52383b8f3b027db11
wulang <wulang@szlhxx.com>
2024-09-18 17:38:16 +0800

    ✨: action option

--- src/main.rs
+++ src/main.rs
@@ -25,5 +25,46 @@ fn main() -> Result<(), PlatformError> {
     });
 
     app.set_todo_list(todo_model.clone().into());
+
+    app.set_option_list({
+        // 无法遍历枚举, 只能手动写
+        let list = vec![
+            OptionItem {
+                action: Actions::Rename,
+                title: "Rename".into(),
+            },
+            OptionItem {
+                action: Actions::Clone,
+                title: "Clone".into(),
+            },
+            OptionItem {
+                action: Actions::Delete,
+                title: "Delete".into(),
+            },
+            OptionItem {
+                action: Actions::Move,
+                title: "Move".into(),
+            },
+        ];
+        // 为什么一定要使用 Rc?
+        let options = Rc::new(VecModel::<OptionItem>::from(list));
+        options.into()
+    });
+
+    app.on_option_action(move |action| match action {
+        Actions::Rename => {
+            println!("修改")
+        }
+        Actions::Clone => {
+            println!("克隆")
+        }
+        Actions::Delete => {
+            println!("删除")
+        }
+        Actions::Move => {
+            println!("移动")
+        }
+    });
+
     app.run()
 }
